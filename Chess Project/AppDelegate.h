eb731fb25198fa40d8f635bfab1b02258452bd2f
Rishi Masand <Rishi@Rishis-Mac-Pro.local>
2015-01-20 23:29:44 -0500

    Initial Commit

---
+++ Chess Project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chess Project
+//
+//  Created by Rishi Masand on 1/20/15.
+//  Copyright (c) 2015 Rishi Masand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Chess Project/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Chess Project
+//
+//  Created by Rishi Masand on 1/20/15.
+//  Copyright (c) 2015 Rishi Masand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
